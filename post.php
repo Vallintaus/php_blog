b7bc16c483c432d71061dee704a7ddf8a4215784
Jusa <jusa.vallintaus@hotmail.com>
2023-12-10 12:34:18 +0200

    Removed faulty images from comments

--- post.php
+++ post.php
@@ -206,9 +206,6 @@ if (isset($_POST['unliked'])) {
 
                 <!-- Comment -->
                 <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
                     <div class="media-body">
                         <h4 class="media-heading"><?php echo $comment_author; ?>
                             <small><?php echo $comment_date; ?></small>
